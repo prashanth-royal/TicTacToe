e632a67e5a6d42556c4f1a267f897818ed6f3f2a
Prashanth <prashanth.royal2046@gmail.com>
2021-05-10 11:00:04 +0530

    Added: Ask user to choose a letter

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,15 +1,24 @@
-	public class TicTacToe {
+		import java.util.Scanner;
+		public class TicTacToe {
 
 		public static void main(String[] args) {
 
-		char[] board = createBoard();
-		}
-
-		private static char[] createBoard() {
-			char[] board = new char[10];
-			for(int i = 0; i < board.length ; i++) {
-				board[i] = ' ';
+			char[] board = createBoard();
+         Scanner userInput = new Scanner(System.in);
+         char userLetter = chooseUserLetter(userInput);
+         char computerLetter = (userLetter == 'X') ? 'O' : 'X';
 			}
-			return board;
-	}
+
+			private static char[] createBoard() {
+				char[] board = new char[10];
+				for(int i = 0; i < board.length ; i++) {
+					board[i] = ' ';
+				}
+				return board;
+			}
+
+			private static char chooseUserLetter(Scanner userInput) {
+				System.out.print("Choose your Letter: ");
+				return userInput.next().toUpperCase().charAt(0);
+	 	}
 }
